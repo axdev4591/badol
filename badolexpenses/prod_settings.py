3cce6e27e117fa280e548490c7346c6c4af35424
Axdev <axelmouele@gmail.com>
2020-11-08 16:38:38 +0100

    update prod setting

--- badolexpenses/prod_settings.py
+++ badolexpenses/prod_settings.py
@@ -1,11 +1,13 @@
-from badolexpenses.settings import *
 import dj_database_url
+
+from badolexpenses.settings import *
+
 
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-DATABASE['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY = 'kr8u^6y3vf$(v#$cb7cb=duto*c-+e41&3*4&d*pm_43m@!%x^'
 
