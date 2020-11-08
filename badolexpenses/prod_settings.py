b85b0b4f695e807a1d901c831deb960db68169da
Axdev <axelmouele@gmail.com>
2020-11-08 16:14:39 +0100

    prd settings

---
+++ badolexpenses/prod_settings.py
@@ -0,0 +1,10 @@
+from badolexpenses.settings import *
+
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+
+SECRET_KEY = 'kr8u^6y3vf$(v#$cb7cb=duto*c-+e41&3*4&d*pm_43m@!%x^'
+
+ALLOWED_HOSTS = ['badol.herokuapp.com'] 
