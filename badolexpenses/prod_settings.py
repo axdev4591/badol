d61136075179bdf034d454d8dca6dd611cb13ed7
Axdev <axelmouele@gmail.com>
2020-12-05 13:49:51 +0100

    create Rest API

--- badolexpenses/prod_settings.py
+++ badolexpenses/prod_settings.py
@@ -1,19 +1,18 @@
 import dj_database_url
 from .settings import *
-
-from decouple import config, Csv
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='s3cr3t_k3y')
+import os
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='127.0.0.1,localhost')
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = False
 
 DATABASES['default'] = dj_database_url.config()
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+
+
+SECRET_KET = os.environ['SECRET_KET']
+
+ALLOWED_HOSTS = ['badol.herokuapp.com'] 
