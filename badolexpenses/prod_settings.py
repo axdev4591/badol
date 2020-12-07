8b2cef928af440d044487f084cbf2d09ad7e3764
Axdev <axelmouele@gmail.com>
2020-12-07 16:41:48 +0100

    Adding new test for expenses

--- badolexpenses/prod_settings.py
+++ badolexpenses/prod_settings.py
@@ -7,7 +7,7 @@ import os
 DEBUG = True
 TEMPLATE_DEBUG = False
 
-#DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
