90e8d94bc976039e3e68690612c925ff6a262208
Axdev <axelmouele@gmail.com>
2020-12-07 15:50:28 +0100

    Adding new test for expenses

--- run_tests.sh
+++ run_tests.sh
@@ -7,12 +7,8 @@ echo '######---------- Creating virtual environement -----------#####\n'
 pipenv shell
 
 echo '######---------- Install requirements -----------####\n'
-
 pip install -r ./requirements.txt
 
-#### Jump to the workspace #####
-echo '######---------- Jump to wokspace -----------####\n'
-cd /var/lib/jenkins/workspace/$JOB_NAME/
 
 echo '######---------- run tests -----------####\n'
 ./manage.py jenkins --enable-coverage
