8739cf04cd19d34f49e7f77432230268b3b7e89b
Axdev <axelmouele@gmail.com>
2020-12-07 17:15:01 +0100

    Adding new test for expenses

--- run_tests.sh
+++ run_tests.sh
@@ -4,18 +4,20 @@
 pwd
 ls
 
-export PIPENV_VENV_IN_PROJECT=1
+#export PIPENV_VENV_IN_PROJECT=1
 
 echo '######---------- Creating virtual environement -----------#####\n'
-pipenv shell
+#pipenv shell
+virtualenv --python=python3.8 env 
 
 echo '######---------- Install requirements -----------####\n'
-pip freeze requirements.txt | sed -e '/^\s*#.*$/d' -e '/^\s*$/d' | xargs -n 1 pip install
+#pip freeze requirements.txt | sed -e '/^\s*#.*$/d' -e '/^\s*$/d' | xargs -n 1 pip install
+cat requirements.txt | sed -e '/^\s*#.*$/d' -e '/^\s*$/d' | xargs -n 1 env/bin/pip install
 
 
 echo '######---------- run tests -----------####\n'
-pip install Django
-./manage.py jenkins --enable-coverage
+#./manage.py jenkins --enable-coverage
+env/bin/python manage.py jenkins
 
 echo '######---------- deactivate virtual env -----------####\n'
 
