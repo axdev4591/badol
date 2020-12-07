7ccc8bc5a05b6acbdea7be38ddadc400d33b2cc4
Axdev <axelmouele@gmail.com>
2020-12-07 14:52:53 +0100

    Adding new test for expenses

--- run_tests.sh
+++ run_tests.sh
@@ -15,7 +15,7 @@ echo '######---------- Jump to wokspace -----------####\n'
 cd /var/lib/jenkins/workspace/$JOB_NAME/
 
 echo '######---------- run tests -----------####\n'
-pytest mexpenses/test --junitxml=./xmlReport/output.xml
+pytest expenses/test --junitxml=./xmlReport/output.xml
 
 echo '######---------- deactivate virtual env -----------####\n'
 deactivate
